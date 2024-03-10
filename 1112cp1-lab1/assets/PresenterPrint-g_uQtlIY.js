55703c401b714c58fcfd1c9766a0794e29b65a63
andyjjrt <60731530+andyjjrt@users.noreply.github.com>
2024-03-10 12:12:28 +0000

    Deploying to gh-pages from @ andyjjrt/slides@0eea6bfc2fe7d433e0299aacc45fa8b8e4ead426 🚀

--- 1112cp1-lab1/assets/PresenterPrint-g_uQtlIY.js
+++ 1112cp1-lab1/assets/PresenterPrint-g_uQtlIY.js
@@ -1,4 +1,4 @@
-import{d as _,u as d,a as p,c as m,b as u,r as h,o as a,e as n,f as t,t as o,g as r,F as f,h as v,n as g,i as x,j as y,k as b,l as k,m as N,_ as w}from"./index-kZQdrFea.js";import{N as P}from"./NoteDisplay-RXwPad_W.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},j=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},C=_({__name:"PresenterPrint",setup(F){d(`
+import{d as _,u as d,a as p,c as m,b as u,r as h,o as a,e as n,f as t,t as o,g as r,F as f,h as v,n as g,i as x,j as y,k as b,l as k,m as N,_ as w}from"./index-AzYu0ZGs.js";import{N as P}from"./NoteDisplay-F2J-PwF1.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},j=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},C=_({__name:"PresenterPrint",setup(F){d(`
 @page {
   size: A4;
   margin-top: 1.5cm;
