8b2de69ca9f7f70221c8e75de73b89927f3b3b93
andyjjrt <60731530+andyjjrt@users.noreply.github.com>
2024-03-12 09:46:48 +0000

    Deploying to gh-pages from @ andyjjrt/slides@9566b6bc28b389d3acd83f0b03fd2c1921b6393c 🚀

---
+++ 1121cp1-week3/assets/slidev/20-ig95f8Pr.js
@@ -0,0 +1,12 @@
+import{_ as i}from"./CodeBlockWrapper-sXHWGpVl.js";import{o as c,c as o,k as e,l as p,m as r,q as u,s as m,A as a,e as s,$ as n}from"../modules/vue-C1svXA8A.js";import{I as _}from"./default-BuGEElKG.js";import{b as d,X as l}from"../index-kSOMcWZa.js";import{p as f,u as h,f as g}from"./context-BIcHlHYe.js";import"../modules/unplugin-icons-Dh6VzJbx.js";import"../modules/shiki-es5OhojV.js";const k=s("h1",null,"oj-cli",-1),A=s("h3",null,"Submit",-1),b=s("pre",{class:"shiki shiki-themes Vitesse Dark Vitesse Light slidev-code",style:{"--shiki-dark":"#dbd7ca","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-txt"},[s("span",{class:"line"},[s("span",null,"$ oj submit hw2 hw2.c")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Submit successfully!")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Getting submission status...")]),n(`
+`),s("span",{class:"line"},[s("span",null,"=================================================")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Result: AC(Accept)              Score: 100")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|ID |Status                |   Time|  Mem| Score|")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|#1 |AC(Accept)            |    0ms|  2MB|    20|")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|#2 |AC(Accept)            |    2ms|  2MB|    20|")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|#3 |AC(Accept)            |   40ms|  2MB|    20|")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|#4 |AC(Accept)            |   75ms|  2MB|    20|")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|#5 |AC(Accept)            |    5ms|  2MB|    20| ")]),n(`
+`),s("span",{class:"line"},[s("span",null,"=================================================")])])],-1),B=s("div",{class:"gradient-bg-orange"},null,-1),C={__name:"20",setup(x){return f(l),h(),(M,S)=>{const t=i;return c(),o(_,u(m(a(g)(a(l),19))),{default:e(()=>[k,A,p(t,r({},{ranges:[]}),{default:e(()=>[b]),_:1},16),B]),_:1},16)}}},N=d(C,[["__file","/@slidev/slides/20.md"]]);export{N as default};
---
+++ 1121cp1-week3/assets/slidev/22-B-smEeYu.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./CodeBlockWrapper-sXHWGpVl.js";import{o as i,c as r,k as a,l as p,m as c,q as u,s as _,A as e,e as s,$ as n}from"../modules/vue-C1svXA8A.js";import{I as d}from"./default-BuGEElKG.js";import{b as m,Z as l}from"../index-kSOMcWZa.js";import{p as f,u as h,f as g}from"./context-BIcHlHYe.js";import"../modules/unplugin-icons-Dh6VzJbx.js";import"../modules/shiki-es5OhojV.js";const k=s("h1",null,"oj-cli",-1),A=s("h3",null,"Rank",-1),C=s("pre",{class:"shiki shiki-themes Vitesse Dark Vitesse Light slidev-code",style:{"--shiki-dark":"#dbd7ca","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-txt"},[s("span",{class:"line"},[s("span",null,"$ oj rank hw2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Your status of hw2 : AC(Accept)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"================================================")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   1~ 10 :  0  |  AC(Accept)             : 8")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  11~ 20 :  0  |  RE(Runtime Error)      : 29")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  21~ 30 :  0  |  PAC(Partial Accepted)  : 54")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  31~ 40 :  1  |  WA(Wrong Answer)       : 81")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  41~ 50 :  0  |  CE(Compilation Error)  : 10")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  51~ 60 :  0  |--------------------------------")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  61~ 70 :  0  |  Total submissions      : 182")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  71~ 80 :  0  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  81~ 90 :  0  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  91~100 : 12  |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"================================================")]),n(`
+`),s("span",{class:"line"},[s("span",null,"For real score ranking,please go to the website.")])])],-1),b=s("div",{class:"gradient-bg-orange"},null,-1),x={__name:"22",setup(P){return f(l),h(),(v,w)=>{const t=o;return i(),r(d,u(_(e(g)(e(l),21))),{default:a(()=>[k,A,p(t,c({},{ranges:[]}),{default:a(()=>[C]),_:1},16),b]),_:1},16)}}},W=m(x,[["__file","/@slidev/slides/22.md"]]);export{W as default};
