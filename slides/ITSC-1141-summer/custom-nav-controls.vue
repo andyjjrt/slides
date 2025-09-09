0045ddfa7767c0e99f676211c19bf63f3a572ee0
andyjjrt <andyjjrt@gmail.com>
2025-09-10 01:19:38 +0800

    slide: update slide

---
+++ slides/ITSC-1141-summer/custom-nav-controls.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-1px opacity-10 bg-current m-1 lg:m-2" />
+  <a class="slidev-icon-btn mr-2" title="Home" href="https://slidev.andyjjrt.cc">
+    <carbon:home />
+  </a>
+</template>
